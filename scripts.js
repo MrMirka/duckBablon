707de0dd2e964b0f5fa3dbba30f93cdb63059fd3
mrmirka <aleshin.dev@gmail.com>
2021-11-10 17:49:27 +0300

    ver13

--- scripts.js
+++ scripts.js
@@ -11,8 +11,8 @@ const createScene = () => {
     const scene = new BABYLON.Scene(engine);
     scene.clearColor = BABYLON.Color3.Black();
 
-    const camera = new BABYLON.ArcRotateCamera('camera', Math.PI/1.4, Math.PI * .3, 6, new BABYLON.Vector3(0,0,0), scene);
-    //camera.attachControl(canvas, true);
+    const camera = new BABYLON.ArcRotateCamera('camera', Math.PI/1.4, Math.PI * .3, 4, new BABYLON.Vector3(0,0,0), scene);
+    camera.attachControl(canvas, true);
     
 
     //const ligth = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
@@ -20,12 +20,14 @@ const createScene = () => {
     pointLigth = new BABYLON.PointLight('pointLigth', new BABYLON.Vector3(0,0,0), scene);
     pointLigth.intensity = 3000;
     
-    /*
-    BABYLON.SceneLoader.Append('obj/', 'duck.gltf', scene, function (scene) {
+    
+    BABYLON.SceneLoader.Append('obj/', 'ball.glb', scene, function (scene) {
         const hdri = BABYLON.CubeTexture.CreateFromPrefilteredData('./img/environment.env', scene);
          scene.enviromentTexture = hdri;
     });
-    */
+    
+
+    /*
     var sphere = BABYLON.Mesh.CreateSphere("sphere1", 64, 2, scene);
     var pbr = new BABYLON.PBRMetallicRoughnessMaterial("pbr", scene);
     sphere.material = pbr;
@@ -33,6 +35,7 @@ const createScene = () => {
     pbr.baseColor = new BABYLON.Color3(1.0, 0.766, 0.336);
     pbr.metallic = 1;
     pbr.roughness = 0.15;
+    */
     
 
     //const hdri = new BABYLON.CubeTexture('./img/environment.env', scene);
@@ -52,8 +55,8 @@ const scene = createScene();
 engine.runRenderLoop(function () {
     triger = Date.now() * 0.0005;
 
-    pointLigth.position.x = Math.sin(triger) * 32;
-    pointLigth.position.z = Math.cos(triger) * 32;
+    pointLigth.position.x = Math.sin(triger) * 5;
+    pointLigth.position.z = Math.cos(triger) * 5;
 
     divFps.innerHTML = engine.getFps().toFixed() + " fps " + engine.webGLVersion;
     scene.render();
