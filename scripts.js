9f56d907f01c809254d27282be4c939996bbe94b
mrmirka <aleshin.dev@gmail.com>
2021-11-10 21:36:09 +0300

    ver28

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-let triger, pointLigth;
+let triger, pointLigth, pointLigth1, pointLigth2, pointLigth3, pointLigth4;
 
 const canvas = document.getElementById('canvas');
 const engine = new BABYLON.Engine(canvas, true);
@@ -18,7 +18,22 @@ const createScene = () => {
     //const ligth = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
 
     pointLigth = new BABYLON.PointLight('pointLigth', new BABYLON.Vector3(0,0,0), scene);
-    pointLigth.intensity = 300;
+    pointLigth.intensity = 30;
+    pointLigth.diffuse = new BABYLON.Color3(1, 0, 0);
+
+    pointLigth1 = new BABYLON.PointLight('pointLigth1', new BABYLON.Vector3(0,1,0), scene);
+    pointLigth1.intensity = 55;
+    pointLigth1.diffuse = new BABYLON.Color3(0, 1, 0);
+
+    pointLigth2 = new BABYLON.PointLight('pointLigth2', new BABYLON.Vector3(0,1.4,0), scene);
+    pointLigth2.intensity = 30;
+
+    pointLigth3 = new BABYLON.PointLight('pointLigth3', new BABYLON.Vector3(0,2,0), scene);
+    pointLigth3.intensity = 10;
+
+    pointLigth4 = new BABYLON.PointLight('pointLigth4', new BABYLON.Vector3(0,2,0), scene);
+    pointLigth4.intensity = 50;
+    pointLigth4.diffuse = new BABYLON.Color3(0, 0, 1);
     
     
     BABYLON.SceneLoader.Append('obj/', 'duck_duck.gltf', scene, function (scene) {
@@ -42,7 +57,7 @@ const createScene = () => {
     var hdri = new BABYLON.HDRCubeTexture("./img/global_env_2.hdr", scene, 128, false, true, false, true);
     //pbr.environmentTexture = hdri;
     scene.environmentTexture = hdri;
-    //scene.environmentIntensity = 0.5;
+    scene.environmentIntensity = 0.5;
    
 
     
@@ -57,6 +72,18 @@ engine.runRenderLoop(function () {
 
     pointLigth.position.x = Math.sin(triger) * 5;
     pointLigth.position.z = Math.cos(triger) * 5;
+
+    pointLigth1.position.x = Math.sin(triger *2) * 15;
+    pointLigth1.position.z = Math.cos(triger *2) * 15;
+
+    pointLigth2.position.x = -Math.sin(triger *3) * 4;
+    pointLigth2.position.z = -Math.cos(triger *3) * 4;
+
+    pointLigth3.position.x = -Math.sin(triger *.3) * 5;
+    pointLigth3.position.z = -Math.cos(triger *.3) * 5;
+
+    pointLigth4.position.x = -Math.sin(triger * 2) * 14;
+    pointLigth4.position.z = -Math.cos(triger * 2) * 14;
 
     divFps.innerHTML = engine.getFps().toFixed() + " fps " + engine.webGLVersion;
     scene.render();
