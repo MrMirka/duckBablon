ad3d1b411afbfff33680b8c91f5fc02dccb84916
mrmirka <aleshin.dev@gmail.com>
2021-11-10 16:13:52 +0300

    ver8

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ const createScene = () => {
     scene.clearColor = BABYLON.Color3.Black();
 
     const camera = new BABYLON.ArcRotateCamera('camera', Math.PI/1.4, Math.PI * .3, 100, new BABYLON.Vector3(0,0,0), scene);
-    //camera.attachControl(canvas, true);
+    camera.attachControl(canvas, true);
     
 
     //const ligth = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
@@ -28,7 +28,7 @@ const createScene = () => {
     var hdri = new BABYLON.HDRCubeTexture("./img/global_env_2.hdr", scene, 128, false, true, false, true);
 
     scene.environmentTexture = hdri;
-    scene.environmentIntensity = 5;
+    scene.environmentIntensity = 0.5;
    
 
     
